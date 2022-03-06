aad8ca0699f7a47d0703aa4fd9f7c8326d73d81d
Nathan Keep <ntkeep@gmail.com>
2022-03-06 15:29:47 -0600

    first commit

---
+++ app/src/main/java/com/example/tvc_drone_java/Calibration.java
@@ -0,0 +1,325 @@
+package com.example.tvc_drone_java;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+public class Calibration extends AppCompatActivity {
+    public static final String DEVICE_EXTRA = "com.example.tvc_drone_java.SOCKET";
+    public static final String DEVICE_UUID = "com.example.tvc_drone_java.uuid";
+    public static final String BUFFER_SIZE = "com.example.tvc_drone_java.buffersize";
+
+    private static final String TAG = "BlueTest5-Controlling";
+    private int mMaxChars = 50000;//Default//change this to string..........
+    private UUID mDeviceUUID;
+    private BluetoothSocket mBTSocket;
+    private ReadInput mReadThread = null;
+
+    private boolean mIsUserInitiatedDisconnect = false;
+    private boolean mIsBluetoothConnected = false;
+
+    private BluetoothDevice mDevice;
+
+    final static String left="1C";// Calibration values have C at the end
+    final static String right="2C";
+    final static String up="3C";
+    final static String down="4C";
+    final static String save="S";
+
+
+    private ProgressDialog progressDialog;
+    Button btnLeft,btnRight, btnUp, btnDown, btnSave;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calibration);
+
+        ActivityHelper.initialize(this);
+        btnLeft=(Button)findViewById(R.id.leftX);
+        btnRight=(Button)findViewById(R.id.rightX);
+        btnUp=(Button)findViewById(R.id.upY);
+        btnDown=(Button)findViewById(R.id.downY);
+        btnSave=(Button)findViewById(R.id.save);
+
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        mDevice = b.getParcelable(MainActivity.DEVICE_EXTRA);
+        mDeviceUUID = UUID.fromString(b.getString(MainActivity.DEVICE_UUID));
+        mMaxChars = b.getInt(MainActivity.BUFFER_SIZE);
+
+        Log.d(TAG, "Ready");
+
+        btnLeft.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(left.getBytes());
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+
+        btnRight.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(right.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+
+        btnUp.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(up.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+
+        btnDown.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(down.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+
+        btnSave.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View arg0) {
+                try {
+                    mBTSocket.getOutputStream().write(save.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+
+                new Calibration.DisConnectBT().execute();
+                Intent intent = new Intent(getApplicationContext(), Controlling.class);
+                intent.putExtra(DEVICE_EXTRA, mDevice);
+                intent.putExtra(DEVICE_UUID, mDeviceUUID.toString());
+                intent.putExtra(BUFFER_SIZE, mMaxChars);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private class ReadInput implements Runnable {
+
+        private boolean bStop = false;
+        private Thread t;
+
+        public ReadInput() {
+            t = new Thread(this, "Input Thread");
+            t.start();
+        }
+
+        public boolean isRunning() {
+            return t.isAlive();
+        }
+
+        @Override
+        public void run() {
+            InputStream inputStream;
+
+            try {
+                inputStream = mBTSocket.getInputStream();
+                while (!bStop) {
+                    byte[] buffer = new byte[256];
+                    if (inputStream.available() > 0) {
+                        inputStream.read(buffer);
+                        int i = 0;
+                        /*
+                         * This is needed because new String(buffer) is taking the entire buffer i.e. 256 chars on Android 2.3.4 http://stackoverflow.com/a/8843462/1287554
+                         */
+                        for (i = 0; i < buffer.length && buffer[i] != 0; i++) {
+                        }
+                        final String strInput = new String(buffer, 0, i);
+
+                        /*
+                         * If checked then receive text, better design would probably be to stop thread if unchecked and free resources, but this is a quick fix
+                         */
+
+                    }
+                    Thread.sleep(500);
+                }
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+        public void stop() {
+            bStop = true;
+        }
+
+    }
+
+    private class DisConnectBT extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {//cant inderstand these dotss
+
+            if (mReadThread != null) {
+                mReadThread.stop();
+                while (mReadThread.isRunning())
+                    ; // Wait until it stops
+                mReadThread = null;
+
+            }
+
+            try {
+                mBTSocket.close();
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            mIsBluetoothConnected = false;
+            if (mIsUserInitiatedDisconnect) {
+                finish();
+            }
+        }
+
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mBTSocket != null && mIsBluetoothConnected) {
+            msg("onPause");
+            new Calibration.DisConnectBT().execute();
+        }
+        Log.d(TAG, "Paused");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mBTSocket == null || !mIsBluetoothConnected) {
+            msg("onResume");
+            new Calibration.ConnectBT().execute();
+        }
+        Log.d(TAG, "Resumed");
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopped");
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+// TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean mConnectSuccessful = true;
+
+        @Override
+        protected void onPreExecute() {
+
+            progressDialog = ProgressDialog.show(Calibration.this, "Hold on", "Connecting");// http://stackoverflow.com/a/11130220/1287554
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+
+            try {
+                if (mBTSocket == null || !mIsBluetoothConnected) {
+                    mBTSocket = mDevice.createInsecureRfcommSocketToServiceRecord(mDeviceUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    mBTSocket.connect();
+                }
+            } catch (IOException e) {
+// Unable to connect to device`
+                // e.printStackTrace();
+                mConnectSuccessful = false;
+
+
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if (!mConnectSuccessful) {
+                Toast.makeText(getApplicationContext(), "Could not connect to device.Please turn on your Hardware", Toast.LENGTH_LONG).show();
+                //finish();
+            } else {
+                msg("Connected to device");
+                mIsBluetoothConnected = true;
+                mReadThread = new Calibration.ReadInput(); // Kick off input reader
+            }
+
+            progressDialog.dismiss();
+        }
+
+    }
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+}
---
+++ app/src/main/java/com/example/tvc_drone_java/Controlling.java
@@ -0,0 +1,368 @@
+package com.example.tvc_drone_java;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tvc_drone_java.ActivityHelper;
+import com.example.tvc_drone_java.MainActivity;
+import com.example.tvc_drone_java.R;
+
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import java.util.UUID;
+import java.util.*;
+
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
+public class Controlling extends AppCompatActivity {
+    private static final String TAG = "BlueTest5-Controlling";
+    private int mMaxChars = 50000;//Default//change this to string..........
+    private UUID mDeviceUUID;
+    private BluetoothSocket mBTSocket;
+    private ReadInput mReadThread = null;
+
+    private boolean mIsUserInitiatedDisconnect = false;
+    private boolean mIsBluetoothConnected = false;
+
+    private BluetoothDevice mDevice;
+
+    final static String raise="R";
+    final static String lower="L";
+    final static String takeOff="T";
+
+
+    private ProgressDialog progressDialog;
+    Button btnRaise,btnLower, btnTakeOff;
+    TextView accelVals, joystickVals;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_controlling);
+
+        ActivityHelper.initialize(this);
+        btnRaise=(Button)findViewById(R.id.raise);
+        btnLower=(Button)findViewById(R.id.lower);
+        btnTakeOff=(Button)findViewById(R.id.takeOff);
+        accelVals=(TextView)findViewById(R.id.accelValues);
+        joystickVals=(TextView)findViewById(R.id.joystickVals);
+
+
+        Intent intent = getIntent();
+        Bundle b = intent.getExtras();
+        mDevice = b.getParcelable(MainActivity.DEVICE_EXTRA);
+        mDeviceUUID = UUID.fromString(b.getString(MainActivity.DEVICE_UUID));
+        mMaxChars = b.getInt(MainActivity.BUFFER_SIZE);
+
+        Log.d(TAG, "Ready");
+
+        btnRaise.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(raise.getBytes());
+
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+
+        btnLower.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(lower.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+        btnTakeOff.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mBTSocket.getOutputStream().write(takeOff.getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }});
+        final JoystickView joystick = (JoystickView) findViewById(R.id.joystick);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                if (strength == 0) {
+                    try {
+                        mBTSocket.getOutputStream().write(("D").getBytes());
+                        return;
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                long maxVal = Math.round(strength*.14);
+                double x = maxVal*Math.cos(Math.toRadians(angle));
+                double y = maxVal*Math.sin(Math.toRadians(angle));
+                double denom;
+//                Log.d(TAG, String.valueOf(maxVal));
+//                Log.d(TAG, String.valueOf(x));
+//                Log.d(TAG, String.valueOf(y));
+//                Log.d(TAG, String.valueOf(angle));
+                if(Math.abs(x) > Math.abs(y)){
+                    denom = x;
+                    x = Math.round(x * (maxVal/(denom+.01)));
+                    y = Math.round(y * (maxVal/(denom+.01)));
+                }
+                else{
+                    denom = y;
+                    y = Math.round(y * (maxVal/(denom+.01)));
+                    x = Math.round(x * (maxVal/(denom+.01)));
+                }
+                if(angle > 135 && angle <= 315){
+                    x *= -1;
+                    y *= -1;
+                };
+//                Log.d(TAG, String.valueOf(x));
+//                Log.d(TAG, String.valueOf(y));
+                String xS = String.valueOf((int)x);
+                String yS = String.valueOf((int)y);
+                String output = xS + ":" + yS + ",";
+                joystickVals.setText(output);
+                try {
+
+                    mBTSocket.getOutputStream().write((output).getBytes());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }, 150);
+
+    }
+
+    private class ReadInput implements Runnable {
+
+        private boolean bStop = false;
+        private Thread t;
+
+        public ReadInput() {
+            t = new Thread(this, "Input Thread");
+            t.start();
+        }
+
+        public boolean isRunning() {
+            return t.isAlive();
+        }
+
+        @Override
+        public void run() {
+            InputStream inputStream;
+
+            try {
+                inputStream = mBTSocket.getInputStream();
+                while (!bStop) {
+                    byte[] buffer = new byte[256];
+                    if (inputStream.available() > 0) {
+                        inputStream.read(buffer);
+                        int i = 0;
+                        /*
+                         * This is needed because new String(buffer) is taking the entire buffer i.e. 256 chars on Android 2.3.4 http://stackoverflow.com/a/8843462/1287554
+                         */
+                        for (i = 0; i < buffer.length && buffer[i] != 0; i++) {
+                        }
+                        final String strInput = new String(buffer, 0, i);
+
+                        accelVals.setText(strInput);
+
+                        /*
+                         * If checked then receive text, better design would probably be to stop thread if unchecked and free resources, but this is a quick fix
+                         */
+
+                    }
+                    Thread.sleep(500);
+                }
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+
+        public void stop() {
+            bStop = true;
+        }
+
+    }
+
+    private class DisConnectBT extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {//cant inderstand these dotss
+
+            if (mReadThread != null) {
+                mReadThread.stop();
+                while (mReadThread.isRunning())
+                    ; // Wait until it stops
+                mReadThread = null;
+
+            }
+
+            try {
+                mBTSocket.close();
+            } catch (IOException e) {
+// TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            mIsBluetoothConnected = false;
+            if (mIsUserInitiatedDisconnect) {
+                finish();
+            }
+        }
+
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        if (mBTSocket != null && mIsBluetoothConnected) {
+            msg("onPause");
+            new DisConnectBT().execute();
+        }
+        Log.d(TAG, "Paused");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        if (mBTSocket == null || !mIsBluetoothConnected) {
+//        if(mBTSocket == null){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            msg("onResume");
+            Thread connect = new Thread(new Runnable(){
+                @Override
+                public void run(){
+                    try {
+                        Thread.sleep(4000);
+                        new ConnectBT().execute();
+                    } catch (Exception e){
+                        e.getLocalizedMessage();
+                    }
+
+                }
+            });
+            connect.run();
+            //new ConnectBT().execute();
+        }
+        Log.d(TAG, "Resumed");
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "Stopped");
+        super.onStop();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+// TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void> {
+        private boolean mConnectSuccessful = true;
+
+        @Override
+        protected void onPreExecute() {
+
+            progressDialog = ProgressDialog.show(Controlling.this, "Hold on", "Connecting");// http://stackoverflow.com/a/11130220/1287554
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) {
+
+            try {
+                if (mBTSocket == null || !mIsBluetoothConnected) {
+                    mBTSocket = mDevice.createInsecureRfcommSocketToServiceRecord(mDeviceUUID);
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    mBTSocket.connect();
+                }
+            } catch (IOException e) {
+// Unable to connect to device`
+                // e.printStackTrace();
+                mConnectSuccessful = false;
+
+
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            if (!mConnectSuccessful) {
+                Toast.makeText(getApplicationContext(), "Could not connect to device.Please turn on your Hardware", Toast.LENGTH_LONG).show();
+                //finish();
+            } else {
+                msg("Connected to device");
+                mIsBluetoothConnected = true;
+                mReadThread = new ReadInput(); // Kick off input reader
+            }
+
+            progressDialog.dismiss();
+        }
+
+    }
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+}
